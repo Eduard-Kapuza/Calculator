0b7709900d6fe96dfb0a97a0001853a49c6363a7
Eduard <edik01101997@mail.ru>
2024-07-12 19:59:55 +0300

    v.1

--- src/calculator/myExceptions/ConstraintNumberException.java
+++ src/calculator/myExceptions/ConstraintNumberException.java
@@ -2,6 +2,6 @@ package calculator.myExceptions;
 
 public class ConstraintNumberException extends RuntimeException{
     public ConstraintNumberException() {
-        super("В введённом выражение присутствует число(а) меньше 1 или больше 10" );
+        super("В введённом выражение присутствует число(а) меньше 1 или больше 10");
     }
 }
--- src/calculator/myExceptions/FormatMathOperationException.java
+++ src/calculator/myExceptions/FormatMathOperationException.java
@@ -2,6 +2,6 @@ package calculator.myExceptions;
 
 public class FormatMathOperationException extends RuntimeException{
     public FormatMathOperationException() {
-        super("формат математической операции не удовлетворяет заданию - два операнда (целые числа) и один оператор (+, -, /, *)");
+        super("формат математической операции не удовлетворяет заданию - два операнда (целые числа) и один оператор (+, -, /, *)" );
     }
 }
